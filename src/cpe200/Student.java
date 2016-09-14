3e49e2f9d5ac0adfa0ff819569c9755e6b104600
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-09-14 14:39:44 +0700

    Uncomplete Ver1

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,14 +1,51 @@
 package cpe200;
 
 public class Student {
+    // declare your attributes here
+    private String student_name;
+    private String student_id;
+    private int yob;
+    private boolean status;
 
     public Student() {
         // implement here
     }
 
     // implement all missing constructors here
+    public Student(){
+
+    }
 
     // implement all get and set methods here
+    public void setName(String name){
+        if(student_name.length()!=0){
+            this.student_name = name;
+        }else{
+            System.out.print("ERROR");
+        }
+    }
+
+    public String setStudent_id(String id){
+    }
+    public int setYearOfBirth(){
+
+    }
+
+    public String getName(){
+        return student_name;
+    }
+
+    public String getStudent_id(){
+        return student_id;
+    }
+
+    public int getYearOfBirth(){
+        return yob;
+    }
+
+    public boolean isActive(){
+        return status;
+    }
 
     @Override
     public String toString() {
@@ -24,7 +61,4 @@ public class Student {
     private boolean isValidYOB(int yob) {
         return true;
     }
-
-    // declare your attributes here
-
 }
