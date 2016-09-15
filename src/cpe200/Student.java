911878e561b686ef7b5ec1e2c6a011876646bfb9
C475L4V3 <satethapan.naowasate@gmail.com>
2016-09-16 02:43:29 +0700

    Finish

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -3,16 +3,52 @@ package cpe200;
 public class Student {
 
     public Student() {
-        // implement here
+        this("John Doe", "560610000", 1990, false);
     }
 
-    // implement all missing constructors here
+    public Student(String name , String id){
+        this(name,id,1990,false);
+    }
+    public Student(String name, String id, int year) {
+        this(name,id,year,false);
+    }
+    public Student(String name, String id, int year, boolean stat){
+        this.name = !name.equalsIgnoreCase("")?name:"John Doe";
+        this.id = (id.matches("^[5][6-9][0][6][1][0-2][0-9][0-9][0-9]$"))?id:"560610000";
+        this.year = (year>1989)?year:1990;
+        this.status = stat;
+    }
 
-    // implement all get and set methods here
-
+    public void setName(String name){
+        if (!name.equals(""))  this.name=name;
+    }
+    public void setStudent_id(String id) {
+        if(id.matches("^[5][6-9][0][6][1][0-2][0-9][0-9][0-9]$")) this.id = id;
+    }
+    public void setYearOfBirth(int year) {
+        if (year> 1989) this.year = year;
+    }
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getStudent_id() {
+        return id;
+    }
+    public int getYearOfBirth() {
+        return year;
+    }
+    public boolean isActive() {
+        return status;
+    }
     @Override
     public String toString() {
-        String o = "Something";
+        String state;
+        if(isActive()==true) state = "ACTIVE";
+        else state="INACTIVE";
+        String o = name + " (" + id + ") was born in " + Integer.toString(year)  + " is an " + state + " student.";
 
         return o;
     }
@@ -25,6 +61,9 @@ public class Student {
         return true;
     }
 
-    // declare your attributes here
+    private String name;
+    private String id;
+    private int year;
+    private boolean status;
 
 }
