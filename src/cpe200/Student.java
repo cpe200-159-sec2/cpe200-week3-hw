389069192153b2d835cee4fc2635a60485654ef6
nod9n <napon_zupat@hotmail.com>
2016-09-16 15:08:24 +0700

    test

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -10,9 +10,9 @@ public class Student {
     }
     public Student(String n, String id, int year , boolean act){
         this.name = !n.equalsIgnoreCase("")?n :"John Doe";
-        String checkID = "^[0-9][6-9][0-9]{3,0}[0-2][0-9]{3,0}$";
+        String checkID = "^[5][6-9][0][6][1][0-9][0-9][0-9][0-9]$";
         if(id.matches(checkID)&&id.length()==9) ID =id;
-        else ID = "560610100";
+        else ID = "560610000";
         if(year >1989) bd = year;
         else bd = 1990;
         active = act;
@@ -21,7 +21,7 @@ public class Student {
         if (!n.equals(""))name =n;
     }
     public  void setStudent_id(String id){
-        String checkID = "^[0-9][6-9][0-9]{3,0}[0-2][0-9]{3,0}$";
+        String checkID = "^[5][6-9][0][6][1][0-9][0-9][0-9][0-9]$$";
         if(id.matches(checkID)&&id.length()==9)ID = id;
     }
     public  void setYearofBirth(int year){
@@ -46,9 +46,9 @@ public class Student {
     @Override
     public String toString() {
         String act;
-        if(active == false) act= "an INACTIVE student.";
+        if(active == false) act= " an INACTIVE student.";
         else act = "an ACTIVE student.";
-        String o = name + " ("+ID+")was born in "+bd+"is"+act;
+        String o = name + " ("+ID+") was born in "+bd+" is"+act;
         return o;
     }
 
