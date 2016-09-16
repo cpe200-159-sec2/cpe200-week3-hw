d8a873ef56c57b1be0703da548b28c964a207312
Panatchakorn <panatchakorn4774@gmail.com>
2016-09-16 16:27:15 +0700

    HW3

--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,30 +1,85 @@
 package cpe200;
 
 public class Student {
+    private String name="John Doe";
+    private String id="560610000";
+    private int yob=1990;
+    private boolean status=false;
 
     public Student() {
-        // implement here
+        this("John Doe","560610000",1990,false);
     }
 
-    // implement all missing constructors here
+    public Student(String name, String id) {
+        setName(name);
+        setStudent_id(id);
+    }
 
-    // implement all get and set methods here
+    public Student(String name, String id, int yob) {
+        setName(name);
+        setStudent_id(id);
+        setYearOfBirth(yob);
+    }
+
+    public Student(String name, String id, int yob, boolean status) {
+        setName(name);
+        setStudent_id(id);
+        setYearOfBirth(yob);
+        setStatus(status);
+    }
+
+    public void setName(String name) {
+        if(name != ""){
+            this.name = name;
+        }
+    }
+
+    public void setStudent_id(String id) {
+        String check = "^[0-9][6-9][0-9]{3,}[0-2][0-9]{3,}$";
+        if (id.matches(check) && id.length() == 9)
+            this.id = id;
+    }
+
+    public void setYearOfBirth(int yob) {
+        if(isValidYOB(yob)){
+            this.yob = yob;
+        }
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudent_id() {
+        return id;
+    }
+
+    public int getYearOfBirth() {
+        return yob;
+    }
+
+    public boolean isActive() {
+        return status;
+    }
+
 
     @Override
     public String toString() {
-        String o = "Something";
+        String o = name+" ("+id+") "+"was born in " +yob+ " is an "+(isActive()?"ACTIVE":"INACTIVE")+" student.";
 
         return o;
     }
 
-    private boolean isValidStudent_id(String id) {
-        return true;
-    }
+
 
     private boolean isValidYOB(int yob) {
-        return true;
+        if (yob > 1989) {
+            return true;
+        }
+        return false;
     }
-
-    // declare your attributes here
-
 }
